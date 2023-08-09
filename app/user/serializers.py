d428e8660217a4e217dcb73f53ca086b2e1bca2f
rivka-yuliia <juliacreadora@gmail.com>
2023-08-10 01:16:50 +0300

    fix missing word

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,4 +16,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
-        return get_user_model().create_user(**validated_data)
+        return get_user_model().objects.create_user(**validated_data)
